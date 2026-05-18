9931e83af0d21bc37119c1af5403f07c5aaa2c9c
carolinemagdy189-cell <carolinemagdy189@gmail.com>
2026-05-19 01:21:45 +0300

    sign up submission + validation + goodle sign up mock

--- sign-up.js
+++ sign-up.js
@@ -32,6 +32,76 @@ const createUser = (user) => {
   const users = getUsers();
   users.push(user);
   saveUsers(users);
-};
+}; 
+
+//  SIGN UP FORM SUBMISSION
+form.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    const fullName = document.getElementById("full_name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirm_password").value;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    // VALIDATION
+    if (!fullName || !email || !password || !confirmPassword) {
+        alert("Please fill in all fields.");
+        return;
+    } 
+    
+    if (!emailRegex.test(email)) {
+        alert("Invalid email format");
+        return;
+    }
+
+    if (password.length < 6) {
+        alert("Password must be at least 6 characters");
+        return;
+    }
+
+    if (password !== confirmPassword) {
+        alert("Passwords do not match");
+        return;
+    }
+
+    if (emailExists(email)) {
+        alert("Email already exists");
+        return;
+    }
+
+    const newUser = {
+        id: Date.now(),
+        fullName,
+        email,
+        password: hashPassword(password),
+        provider: "local"
+    };
+    
+    createUser(newUser);
+    setLoggedUser(newUser);
+
+    alert("Account created successfully!");
+    window.location.href = "index.html"; // Redirect to homepage or dashboard
+});
 
 
+// GOOGLE SIGN UP (MOCK)
+googleBtn.addEventListener('click', () => {
+    const googleUser = {
+        id: Date.now(),
+        fullName: "Google User",
+        email: "googleuser@gmail.com",
+        password: hashPassword("googlepassword"),
+        provider: "google"
+    };
+
+
+  const users = getUsers();
+
+  if (!users.some((u) => u.email === googleUser.email)) {
+    createUser(googleUser);
+  }
+    setLoggedUser(googleUser);
+    alert("Account created successfully!");
+    window.location.href = "index.html"; // Redirect to homepage or dashboard
+});
