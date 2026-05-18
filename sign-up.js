6ab0bcda2880b5a879ddb5a927bbc1baad601cf6
carolinemagdy189-cell <carolinemagdy189@gmail.com>
2026-05-19 00:26:03 +0300

    prevent logged user from open again + password encyption

--- sign-up.js
+++ sign-up.js
@@ -1,4 +1,16 @@
-const form = document.querySelector("form");
+console.log("Sign-up script loaded");
+const form = document.querySelector('form');
+const googleBtn = document.querySelector("button[type='button']");
 
-console.log(form);
-console.log("hello")
+googleBtn.addEventListener('click', () => {
+    // Handle Google sign-up logic here
+});
+// Prevent logged user from opening signup again
+const loggedUser = JSON.parse(localStorage.getItem('loggedUser'));
+if (loggedUser) {
+    window.location.href = 'index.html'; // Redirect to homepage or dashboard
+}
+// password encryption
+const hashPassword = (password) => btoa(password);    ; 
+
+
