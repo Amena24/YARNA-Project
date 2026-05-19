215c5ca3b822546f508e13c72f26a600cab355f4
carolinemagdy189-cell <carolinemagdy189@gmail.com>
2026-05-19 06:40:18 +0300

    repair navigation form sign in to forget pass

--- sign-in.js
+++ sign-in.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector("form");
   const googleBtn = document.querySelector("button[type='button']");
   const goToSignup = document.getElementById("goToSignup");
+  const forgotPassword = document.getElementById("forgotPassword");
   const toast = document.getElementById("toast");
 
   // TOAST SYSTEM
@@ -96,6 +97,11 @@ document.addEventListener("DOMContentLoaded", () => {
       window.location.href = "index.html";
     }, 2000);
   });
+// GO TO FORGET PASS
+forgotPassword?.addEventListener("click", (e) => {
+  e.preventDefault();
+  window.location.href = "forget-pass.html";
+});
 
   // GO TO SIGNUP
   goToSignup?.addEventListener("click", (e) => {
